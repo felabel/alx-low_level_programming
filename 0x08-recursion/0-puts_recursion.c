0c6a5503cb8d517ebe2441044194aa46672cd788
Felicity Essien <felicityabel2016@gmail.com>
2023-09-28 15:31:45 -0700

    correct code

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,17 @@
 #include "main.h"
+/**
+ * _puts_recursion - takes in string to be reversed
+ *@s: STRING TO BE REVERSED
+ *Return: Always 0 (Success)
+ */
 
 void _puts_recursion(char *s)
 {
-if (*s == '\0')
-{
-_putchar('\n');
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
-else
-{
-_puts_recursion(s + 1);
-_putchar(*s);
-};
