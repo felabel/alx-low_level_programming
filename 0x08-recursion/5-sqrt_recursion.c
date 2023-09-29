48a7fb7554b1b66f63116b319f0b8417d04ad754
Felicity Essien <felicityabel2016@gmail.com>
2023-09-28 17:48:53 -0700

    corrected square root function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,30 +1,29 @@
 #include "main.h"
+/**
+ * sqrt_a - Main Entry
+ * @a: input
+ * @b: input
+ * Return: 0
+ */
+int sqrt_a(int a, int b)
+{
+	if (b * b == a)
+	{
+		return (b);
+	}
+	else if (b * b > a)
+	{
+		return (-1);
+	}
+	return (sqrt_a(a, b + 1));
+}
 
 /**
- * _sqrt_recursion - Calculate the natural square root of a number.
- * @n: The input number.
- * Return: The natural square root of n, or -1 if it does not have a natural square root.
- * _sqrt_helper - Helper function to find the natural square root using recursion.
- * @n: The input number.
- * @guess: The current guess for the square root.
- * Return: The natural square root of n, or -1 if it does not have a natural square root.
+ * _sqrt_recursion - Main Entry
+ * @n: input
+ * Return: 0
  */
-
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-return (-1);
-else if (n == 0 || n == 1)
-return (n);
-else
-return (_sqrt_helper(n, 2));
-}
-int _sqrt_helper(int n, int guess)
-{
-if (guess * guess == n)
-return (guess); 
-else if (guess * guess > n)
-return (-1);
-else
-return (_sqrt_helper(n, guess + 1));
+	return (sqrt_a(n, 0));
 }
