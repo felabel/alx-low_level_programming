08fd88aa91025133f49638b94e66eac4edc5ad14
Felicity Essien <felicityabel2016@gmail.com>
2023-09-28 18:47:29 -0700

    wildcard

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,50 +1,28 @@
 #include "main.h"
 /**
- * long_1 - checks if a string is a palindroe
- * @x: input
+ * wildcmp - Main Entry
+ * @s1: input
+ * @s2: input
  * Return: 0
  */
-
-int long_1(char *x)
+int wildcmp(char *s1, char *s2)
 {
-	if (*x != '\0')
+	if (*s1 == '\0')
 	{
-		return (1 + long_1(x + 1));
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
-
-}
-/**
- * compare - compaes the string
- * @x: input
- * @y: input
- * Return: 0
- */
-int compare(char *x, int y)
-{
-	if (y <= 0)
-	{
-		return (1);
-	}
-	if (*x == *(x + (y - 1)))
-	{
-		return (compare(x + 1, y - 2));
-	}
-	else
-	{
-		return (0);
-	}
-}
-
-/**
- * is_palindrome - Main Entry
- * @x: input
- * Return: 0
- */
-int is_palindrome(char *x)
-{
-	int y;
-
-	y = long_1(x);
-	return (compare(x, y));
 }
