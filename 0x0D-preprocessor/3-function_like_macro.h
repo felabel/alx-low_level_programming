6968f2064ec3e9fdd653a8601535d8c7eef6e1f9
Felicity Essien <felicityabel2016@gmail.com>
2023-10-10 14:33:13 -0700

    task 3 and 4

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACR0_H
+#define FUNCTION_LIKE_MACR0_H
+
+/**
+ * file: 3-function_like_macro.h
+ * @x: input
+ * Desc: header file
+ */
+
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+
+#endif
