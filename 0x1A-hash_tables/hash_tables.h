c193631c250183c4243998b50cedd72069849ab9
Felicity Essien <felicityabel2016@gmail.com>
2024-01-02 17:46:48 -0800

    has table task 0

---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,10 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+shash_table_t *shash_table_create(unsigned long int size);
+
+#endif /* HASH_TABLES_H */
